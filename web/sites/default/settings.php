a3b6cc81d8efb4b9e346d1d64a784444b6ec63f4
Al Zee <z@alz.ee>
2022-01-21 11:46:24 +0800

    nth

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -770,7 +770,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'zszz',
+  'database' => 'zmall',
   'username' => 'root',
   'password' => 'toor',
   'prefix' => '',
